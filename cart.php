eec6844a8cb5e64114d8018625f8e949d209165a
Jason2non <43177493+Jason2non@users.noreply.github.com>
2024-01-17 23:48:19 +0300

    Add files via upload

--- cart.php
+++ cart.php
@@ -1,8 +1,70 @@
+<?php
+session_start();
+
+require('connect.php');
+
+/*if(isset($_POST['add'])){
+    if(isset($_SESSION['cart']))
+    {
+        $item_id = array_column($_SESSION['cart'], "ID");
+        if(!in_array($_POST['ID'], $item_id))
+        {
+           $count = count($_SESSION['cart']);
+           $message = "<div class=\"alert alert-success\" style=\"text-align:center; \">Product added</div>";
+           $items_arr = array
+            (
+                'product_id' => $_POST['ID'],
+                'item_name'  => $_POST['hidden_name'],
+                'item_image' => $_POST['hidden_image'],
+                'item_quant' => $_POST['quant'],
+                'item_model' => $_POST['hidden_model'],
+                'item_price' => $_POST['hidden_price'],
+                'item_desc'  => $_POST['hidden_decrip'],
+   
+            );
+            $_SESSION['cart'][$count] = $items_arr;
+            echo "<script>alert(\"Product added\")</script>";
+            echo "<script>window.location=\"showroom.php\"</script>";
+        }
+        else
+        {
+            echo "<script>alert(\"Product already added\")</script>";
+
+        }
+    }
+    else
+    {
+            $items_arr = array
+            (
+            'product_id' => $_POST['ID'],
+            'item_name'  => $_POST['hidden_name'],
+            'item_image' => $_POST['hidden_image'],
+            'item_quant' => $_POST['quant'],
+            'item_model' => $_POST['hidden_model'],
+            'item_price' => $_POST['hidden_price'],
+            'item_desc'  => $_POST['hidden_decrip'],
+
+            );
+            $_SESSION['cart'][0] = $items_arr;
+    }
+}
+if (isset($_GET['action'])){
+    if($_GET['action'] == 'delete'){
+        foreach ($_SESSION['cart'] as $key => $value) {
+            if($value['product_id'] == $_GET['ID']){
+                unset($_SESSION['cart'][$key]);
+            }
+        }
+    }
+}
+*/
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/bot\bot/css/bootstrap.css">
     <title>Shopping Cart</title>
     <style>
         body {
@@ -17,6 +79,9 @@
             color: #fff;
             padding: 10px;
             text-align: center;
+        }
+        img{
+            border-radius: 5px;
         }
 
         #cart-container {
@@ -55,29 +120,95 @@
             border-radius: 4px;
             cursor: pointer;
         }
+        nav{
+            display: flex;
+            justify-content: center;
+            margin-top: 5px;
+        }
+        nav ul {
+            list-style: none;
+            padding: 0;
+        }
+
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+            font-family: deja vu;
+            font-weight: bold;
+        }
+
+        nav a {
+            color: #333;
+            text-decoration: none;
+        }
+        nav a:hover{
+            border-bottom: 1.5px solid;
+            border-color: #333;
     </style>
 </head>
 <body>
 
     <header>
-        <h1>SHOPPING CART</h1>
+        <h1 style="font-family: deva ju">SHOPPING CART</h1>
     </header>
+     <nav class="nav nav-bar">
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="showroom.php">BUY CAR</a></li>
+                <li><a href="explorecar.php">EXPLORE CARS</a></li>
+                <li><a href="cart.php">CART</a></li>
+            </ul>
+    </nav>
+    </div>
+    <div class="table responsive" style="clear: both;">
+        <!--<h1 class="text-center">Shopping Wallet</h1>-->
+        <table class="table table-bordered table-stripped" style="width: 90%; margin-left: 5%; margin-top:3%;">
+            <tr style="text-align: right; font-family: deja vu; font-size: 15px">
+                <th width="30%" style="text-align: left; font-family: deja vu;">IMAGE</th>
+                <th width="15%">PRODUCT NAME</th>
+                <th width="11%">QUANTITY</th>
+                <th width="30%">TOTAL PRICE</th>
+                <th width="50%"></th>
+            </tr>
+            <?php 
+            $fetch = mysqli_query($sql, "SELECT * FROM `checkout`")or die("This is the error". mysqli_error($sql));
+            while ($row = mysqli_fetch_array($fetch)) {
+              /* if(!empty($_SESSION['cart']))
+               {
+                $total = 0;
+                foreach ($_SESSION['cart'] as $key => $value) {
+                    */
+            ?>
+            <tr style="">
+                <td><img src="admin/uploads/<? echo $row['p_image']?>" height="40px" width="60px"></td>
+                <td style="font-family: deja vu; font-size: 15px;text-align: right"><? echo strtoupper($row['p_name'])?></td>
+                <td style="font-family: deja vu;text-align: right"><? echo $row['p_quant']?></td>
+                <td style="font-family: deja vu; font-size: 17px;text-align: right">&dollar; <? echo number_format($row['p_price'],2)?></td>
+                <td><a href="cart.php?action=delete&ID=<?echo $value['product_id']?>" class="btn btn-danger btn-block">Remove Item</a></td>
+            </tr>
+            <?php
+            //$total = $total + $value['item_price']*$value['item_quant'];
+            $total = 0;
+            $total = $total + $row['p_price'];
 
-    <div id="cart-container">
-        <div class="cart-item">
-            <img src="img/car3.jpg" alt="#">
-            <div>
-                <h3> </h3>
-                <p>Price: $30000</p>
-                <p>VOLVO</p>
-            </div>
-        </div>
-    <div class="cart-total">
-            <p>Total: $15000</p>
-        </div>
-
-        <button>CHECKOUT</button>
+        }
+             
+            ?>
+            <tr>
+                <td colspan="3" style="text-align: right; font-weight: bold; font-size: 20px;">Total</td>
+                <td style="text-align: right;font-family: deja vu; font-size: 17px;">&dollar; <?php echo number_format($total,2) ?></td>
+                <td colspan="5" style="text-align: right;"><a href="checkout.php" class="btn btn-warning">Checkout</a></td>
+            </tr>
+            <tr>
+                
+            </tr>
+            <?php
+        ?>
+        </table>
+    <div style="margin-bottom: 100px">
+        <!--<?php// include('include/footer.php');?>-->
     </div>
 
 </body>
 </html>
+
--- showroom.php
+++ showroom.php
@@ -1,50 +1,32 @@
 <?php
 require('connect.php');
 session_start();
-$fetch =  mysqli_query($sql,"SELECT * FROM `car` ORDER BY `ID` asc")or die(mysqli_error($sql));
 if(isset($_POST['add'])){
-    if(isset($_SESSION['cart'])){
-        $item_id = array_column($_SESSION['cart'], "ID");
-        if(!in_array($_GET['ID'], $item_id)){
-            $count = count($_SESSION['cart']);
-            $item_array = array(
-                'product_id'     => $_GET['ID'],
-                'item_name'      => $_POST['hidden_name'],
-                'item_quantity'  => $_POST['quant'],
-                'item_model'     => $_POST['hidden_model'],
-                'item_price'     => $_POST['hidden_price'],
-                'item_desc'      => $_POST['hidden_decrip'],
-            );
-            $_SESSION['cart'][$count]=$item_array;
-            echo "<script>window.location=\"showroom.php\"</script>";
-        }
-        else{
-            echo "Product already added into the cart";
-        }
+    $id  = $_POST['ID'];
+    $name  = $_POST['hidden_name'];
+    $model = $_POST['hidden_model'];
+    $image = $_POST['hidden_image'];
+    $quant = $_POST['quant'];
+    $price = $_POST['hidden_price'] * $_POST['quant'];
+    $descr = $_POST['hidden_decrip'];
+
+    $fetch_data = mysqli_query($sql, "SELECT count(*) FROM `car` WHERE `ID`='$id'")or die("Cannot get into the database error!");
+    if(mysqli_num_rows($fetch_data) > 0){
+        $insert = mysqli_query($sql,"INSERT INTO `checkout` (`ID`,`p_name`,`p_image`,`p_quant`,`p_model`,`p_price`,`p_desc`)VALUES('','$name','$image','$quant','$model','$price','$descr')")or die("Theres an error: ".$sql);
+        echo "<div class=\"alert alert-success\" style=\"text-align:center; margin-top:5px;\">Product added</div>";
     }
     else{
-        $item_array = array(
-                'product_id'    => $_GET['ID'],
-                'item_name'     => $_POST['hidden_name'],
-                'item_quantity' =>  $_POST['quant'],
-                'item_model'    => $_POST['hidden_model'],
-                'item_price'    => $_POST['hidden_price'],
-                'item_desc'     => $_POST['hidden_decrip'],
-            );
-            $_SESSION['cart'][0]=$item_array;
+        
+        echo "Product already added";
     }
 }
- if(isset($_GET['action'])){
-        if($_GET['action'] == "delete"){
-            foreach ($_SESSION['cart'] as $key => $value) {
-                if($value['product_id'] == $_GET['ID']){
-                    unset($_SESSION['cart'][$key]);
-                    echo "<script>alert(\"Product Removed\")</script>";
-                }
-                # code...
-            }
-        }
-    }
+$fetch_check = mysqli_query($sql, "SELECT * FROM `checkout`")or die("Error fetching data");
+$fetch_array = mysqli_num_rows($fetch_check);
+//$array = count($fetch_array);
+
+
+//$array = mysqli_fetch_array($fetch_check);
+//$array = mysqli_num_rows($fetch_check);
 ?>
 <!DOCTYPE html>
 <html>
@@ -52,35 +34,41 @@ if(isset($_POST['add'])){
     <title>CarShow Room</title>
     <link rel="stylesheet" type="text/css" href="css/showroom.css">
     <link rel="stylesheet" type="text/css" href="css/bot\bot/css/bootstrap.css">
+    <link rel="stylesheet" type="text/css" href="css/bot\bot/css/bootstrap.min.css">
 </head>
 <body>
     <header>
         <h1>CASPAL SHOW ROOM</h1>
         <nav>
             <ul>
-                <li><a href="index.php">HOME</a></li>
-                <li><a href="buycar.php">BUY CAR</a></li>
-                <li><a href="explorecar.php">EXPLORE CARS</a></li>
-                <li><a href="cart.php">CART</a></li>
+                <li><a href="index.php">| Home |</a></li>
+                <li><a href="buycar.php">| Buy Car |</a></li>
+                <li><a href="explorecar.php">| Explore Cars |</a></li>
+                <li><a href="cart.php">| Cart <sup><? echo $fetch_array ?></sup> |</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
         <section class="product-list">
-                  <?php while($row = mysqli_fetch_array($fetch))
-    {?>
+                  <?php
+                      $fetch =  mysqli_query($sql,"SELECT * FROM `car` ORDER BY `ID` asc")or die(mysqli_error($sql));
+                      while($row = mysqli_fetch_array($fetch))
+
+                    {?>
             <!-- Product listings go here -->
-            <form action="showroom.php?action=add&ID=<? echo $row['ID']?>" method="POST">
+            <form action="showroom.php" method="POST">
                     <div class="product" style="background-color: ; color:black">
                         <img src="admin/uploads/<? echo $row['img'] ?>" alt="Loading..." height="200px"><br><br><br>
                         <h2 style="font-family:C059; font-size: 15px; color: black"><? echo strtoupper($row['model']) ?> <? echo strtoupper($row['carname']) ?></h2>
                         <label>Quantity</label><br>
                         <input type="number" name="quant" class="form-control" value="1"><br>
                         <p style="color: black; font-family: DejaVu Sans;">Price: $ <? echo number_format($row['price'],2)?></p>
-                        <input type="hidden" name="hidden_name" value="<? echo $row['carname']?>">
-                        <input type="hidden" name="hidden_model" value="<? echo $row['model']?>">
-                        <input type="hidden" name="hidden_price" value="<? echo $row['price']?>">
+                        <input type="hidden" name="ID"    value="<? echo $row['ID']?>">
+                        <input type="hidden" name="hidden_name"   value="<? echo $row['carname']?>">
+                        <input type="hidden" name="hidden_model"  value="<? echo $row['model']?>">
+                        <input type="hidden" name="hidden_image"  value="<? echo $row['img']?>">
+                        <input type="hidden" name="hidden_price"  value="<? echo $row['price']?>">
                         <input type="hidden" name="hidden_decrip" value="<? echo $row['descr']?>">
                         <input type="submit" name="add" value="Add to cart" class="btn btn-warning btn-block">
                         
@@ -89,48 +77,6 @@ if(isset($_POST['add'])){
             <?php
               }
         ?>
-        <div style="clear:both"></div>
-        <h3 class="title2">Shoping Cart Details</h3>
-        <div class="table">
-         <table class="table table-bordered table-stripped">
-            <tr>
-                <th width="30%">Product Name</th>
-                <th width="10%">Quantity</th>
-                <th width="13%">Price Details</th>
-                <th width="10%">Total Price</th>
-                <th width="17%">Remove Item</th>
-        </div>
-        <?php
-            if(!empty($_SESSION['cart'])){
-                $total = 0;
-                foreach ($_SESSION['cart'] as $key => $value) {
-        ?>
-        <tr>
-            <td><? echo $value['item_name']?></td>
-            <td><? echo $value['item_quantity']?></td>
-            <td><? echo number_format($value['item_price'])?></td>
-            <td><? echo number_format($value['item_quantity'] * $value['item_price'],2)?></td>
-            <td><a href="showroom.php?action=delete&ID=<? echo $value['product_id']?>" class="btn btn-danger btn-block">Remove</a></td>
-        </tr>
-        <?php
-    }
-    ?>
-    <tr>
-           <td colspan="2"></td>
-            <td>Total</td>
-            <td><?php echo number_format($total = $total + $value['item_quantity'] * $value['item_price'],2)?></td>
-            <td><a href="showroom.php?action=delete&ID=<? echo $row['ID']?>" class="btn btn-info btn-block" style="color:white;">Checkout</a></td>
-    </tr>
-<?php
- }
-?>
-        <!--<tr>
-            <td colspan="2"></td>
-            <td>Total</td>
-            <td><?php //echo number_format($total = $total + $value['item_quantity'] * $value['item_price'],2)?></td>
-            <td><a href="showroom.php?action=delete&ID=<? echo $row['ID']?>" class="btn btn-info btn-block" style="color:white;">Checkout</a></td>
-        </tr>-->
-</table>
         </section>
     </main>
     
